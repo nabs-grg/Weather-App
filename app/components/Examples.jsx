16a404e06862dfaec696aa84e512d3ac3c77df62
nabs-grg <narbu.grg@gmail.com>
2016-12-31 00:53:36 +0000

    check

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,6 +4,7 @@ var Examples = (props) => {
 	return (
 			<div>
 				<h2>Examples</h2>
+				<p>Welcome to example component</p>
 			</div>
 		)
 }
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 
-// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req,res,next){
-	if(req.header['x-forwarded-proto'] === 'http'){
+	if(req.headers['x-forwarded-proto'] === 'http'){
 		next();
 	} else {
 		res.redirect('http://' + req.hostname + req.url);
@@ -15,5 +14,5 @@ app.use(function (req,res,next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port 3000' + PORT);
+  console.log('Express server is up on port 3000');
 });
