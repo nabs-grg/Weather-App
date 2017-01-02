ad60d392a0264deff70a4150d3ad45a01e05299a
nabs-grg <narbu.grg@gmail.com>
2017-01-02 14:42:15 +0000

    grid layout for the main page is aded and two example location is added for the weather app

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => { 
 	return (
 			<div>
-				<h2>Examples</h2>
-				<p>Welcome to example component</p>
+				<h1 className="text-center">Examples</h1>
+				<p>You can try out the few examples location provided below:</p>
+				<ol>
+					<li>
+						<Link to='/?location=London'>London, UK</Link>
+					</li>
+					<li>
+						<Link to='/?location=NewYork'>New York, USA</Link>
+					</li>
+				</ol>
 			</div>
 		)
 }
